e3c55e16c1cbea23f72c657fe9edbaf989b26f6d
dagas_sa <dagassacoop@gmail.com>
2024-09-07 17:40:10 -0400

    remove all source file for creating clean branches

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-
-  return (
-    <section className="text-center p-8 bg-white rounded-lg shadow-lg space-y-6">
-      <h1 className="text-3xl font-bold">Welcome to the Homepage!</h1>
-    </section>
-  );
-}
